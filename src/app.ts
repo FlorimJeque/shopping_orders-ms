cf55c92b2b800e89733e90a0d33d398dfef8122f
Florim Jeque <jeque.developer@gmail.com>
2020-09-14 12:39:29 +0200

    added dotenv config

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import dotenv from 'dotenv';
 
 import routes from './routes/routes';
+
+dotenv.config();
+
 const app = express();
 
 app.use(cors());
