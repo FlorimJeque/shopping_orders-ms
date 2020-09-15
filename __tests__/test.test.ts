08a0b1ca79367af69741e9fea5e78b62771d43a5
Florim Jeque <jeque.developer@gmail.com>
2020-09-15 12:48:04 +0200

    added test boilerplate

--- __tests__/test.test.ts
+++ __tests__/test.test.ts
@@ -1,6 +1,13 @@
 /* eslint-disable no-undef */
+import supertest from 'supertest';
+/* import faker from 'faker'; */
+
+import app from '../src/app';
+
+const api = supertest(app);
 describe('Check express', () => {
-  it('Express rocks', () => {
-    expect(0).toBe(0);
+  it('Express rocks', async () => {
+    const response = await api.get('/').send();
+    expect(response.status).toEqual(200);
   });
 });
---
+++ src/services/mail.ts
@@ -0,0 +1,33 @@
+const sendgrid = require('@sendgrid/mail');
+const fs = require('fs');
+const template = require('../utils/template');
+
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+
+export default {
+  sendMail: async (params: IMailParams) => {
+    const msg = {
+      to: params.to,
+      from: 'jeque.developer@gmailcom',
+      subject: params.subject,
+      text: params.text,
+      html: template.compileHtmlData(
+        fs.readFileSync(`./resources/templates/mail/${params.templateFile}`, 'utf-8'),
+        params.templateData
+      ),
+      attachments: params.attachments,
+    };
+
+    return await sendgrid.send(msg);
+  },
+};
+
+interface IMailParams {
+  to: string;
+  subject: string;
+  from: string;
+  text: string;
+  templateFile: string;
+  templateData: object;
+  attachments: Array<string>;
+}
